a037fb575c7560e20ac7b96fc7d237e8e9fe361f
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-10-19 18:17:55 +0900

    ポイントのバリデーションを設置

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,3 +1,15 @@
 class Exchange < ApplicationRecord
   belongs_to :user
+
+  validates :point, presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 10_000 }
+  validate :less_than_possession_points
+
+  private
+
+  def less_than_possession_points
+    if user.point_count < point
+      errors.add(:point, 'は所持ポイントより大きな値は設定できません')
+    end
+  end
 end
