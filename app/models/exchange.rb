52ac6359f67be6bdcf439b692463169b49eafad6
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-10-20 01:11:15 +0900

    ポイント申請時に所持ポイントから申請ポイント分の引き算を行う

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -7,11 +7,18 @@ class Exchange < ApplicationRecord
             numericality: { only_integer: true, greater_than_or_equal_to: 10_000 }
   validate :less_than_possession_points
 
+  after_create :update_point_balance
+
   private
 
   def less_than_possession_points
     if user.point_count < point
       errors.add(:point, 'は所持ポイントより大きな値は設定できません')
     end
+  end
+
+  def update_point_balance
+    point_diff = user.point_count - point
+    user.update(point_count: point_diff)
   end
 end
