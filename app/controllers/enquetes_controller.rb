5ffe9bba965bb99a04afea2d93429fbb7fdde7dd
stobita <tobita.pc@gmail.com>
2017-10-24 00:00:47 +0900

    アンケート回答時に紹介者にポイント付与

--- app/controllers/enquetes_controller.rb
+++ app/controllers/enquetes_controller.rb
@@ -10,6 +10,7 @@ class EnquetesController < ApplicationController
     @enquete = Reservation.find(params[:reservation_id]).enquetes.build(enquete_params)
     @enquete.answer_date = Date.today
     if @enquete.save
+      introduction_point_add
       redirect_to mypage_url, notice: 'アンケートを送信しました'
     else
       render :new
@@ -22,5 +23,15 @@ class EnquetesController < ApplicationController
     params.fetch(:enquete, {}).permit(
       enquete_answers_attributes: [:enquete_item_id, :string_value, :integer_value, :boolean_value]
     )
+  end
+
+  # 友人紹介から利用したユーザーの場合、紹介者にポイント付与
+  def introduction_point_add
+    introduction = Introduction.registered&.find_by(introduced_id: current_user.id)
+    if introduction.present?
+      introduction.user.point_count += 3000
+      introduction.user.save!
+      introduction.provided!
+    end
   end
 end
