023f11170c8e6ff633d6e321c6fe036ac1046221
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-10-28 01:51:53 +0900

    アンケート送信時にポイント申請レコードが作成されるコールバック作成

--- app/models/apply_point.rb
+++ app/models/apply_point.rb
@@ -1,6 +1,12 @@
 class ApplyPoint < ApplicationRecord
+  EXCHANGE_RATE = 0.1
+
   belongs_to :reservation
   belongs_to :user
 
   enum status: { pending: 0, done: 1, remand: 2, resubmit: 3 }
+
+  def set_point
+    self.point = (use_price * EXCHANGE_RATE).to_i
+  end
 end
--- app/models/enquete.rb
+++ app/models/enquete.rb
@@ -7,4 +7,17 @@ class Enquete < ApplicationRecord
   validates :answer_date, presence: true
 
   accepts_nested_attributes_for :enquete_answers
+
+  after_create :create_apply_point_record
+
+  private
+
+  def create_apply_point_record
+    apply_point = reservation.build_apply_point(
+      user: reservation.user,
+      use_price: reservation.sum_price
+    )
+    apply_point.set_point
+    apply_point.save
+  end
 end
