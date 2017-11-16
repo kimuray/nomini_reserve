7c2fb5d6891c1d84fba24a430b132f96ff8e7fbd
stobita <tobita.pc@gmail.com>
2017-11-16 22:55:40 +0900

    詳細画面/更新/キャンセル実装

--- app/controllers/shop_page/reservations_controller.rb
+++ app/controllers/shop_page/reservations_controller.rb
@@ -4,6 +4,10 @@ class ShopPage::ReservationsController < ShopPageController
     @reservations = current_shop.reservations.page(params[:page])
   end
 
+  def show
+    @reservation = Reservation.find(params[:id])
+  end
+
   def new
     @reservation = current_shop.reservations.build
   end
@@ -13,11 +17,33 @@ class ShopPage::ReservationsController < ShopPageController
     @reservation.user = User.find(params[:reservation][:customer_id])
     @reservation.shop = current_shop
     if params[:back].blank? && @reservation.save
+      @reservation.done!
       send_reservation_mail
       redirect_to shop_page_root_path, notice: '予約を作成しました。'
     else
       render :new
     end
+  end
+
+  def edit
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_params)
+      send_update_mail
+      redirect_to shop_page_reservation_url(@reservation), notice: '予約内容を変更しました。'
+    else
+      render :edit
+    end
+  end
+
+  def cancel
+    @reservation = Reservation.find(params[:id])
+    @reservation.canceled!
+    send_cancel_mail
+    redirect_to shop_page_root_path, notice: '予約をキャンセルしました'
   end
 
   def confirm
@@ -48,5 +74,14 @@ class ShopPage::ReservationsController < ShopPageController
     ReservationMailer.notice_reservation_to_user_from_shop(@reservation).deliver_now
   end
 
+  def send_update_mail
+    ReservationMailer.update_reservation_to_nomini_from_shop(@reservation).deliver_now
+    ReservationMailer.update_reservation_to_user_from_shop(@reservation).deliver_now
+  end
+
+  def send_cancel_mail
+    ReservationMailer.cancel_reservation_to_nomini_from_shop(@reservation).deliver_now
+    ReservationMailer.cancel_reservation_to_user_from_shop(@reservation).deliver_now
+  end
 
 end
