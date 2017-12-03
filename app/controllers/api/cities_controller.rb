ccae07e7c6626ad646b75d04dc342e79b60a3357
stobita <tobita.pc@gmail.com>
2017-12-03 16:41:46 +0900

    市区町村をid順で取得

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -1,5 +1,5 @@
 class Api::CitiesController < ApiController
   def index
-    @cities = City.where(prefecture_id: params[:prefecture_id])
+    @cities = City.where(prefecture_id: params[:prefecture_id]).order(:id)
   end
 end
