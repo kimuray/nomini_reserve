01c2fb2010a10837d9eac5d990a83d6ce048215f
stobita <tobita.pc@gmail.com>
2017-12-09 16:31:46 +0900

    定期課金画面に名前と電話番号の入力を追加

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,39 @@
 class SubscriptionsController < ApplicationController
   def new
+    @user = current_user
     @subscription = Subscription.new
   end
 
   def create
-    subscription = current_user.build_subscription
-    token = PayjpApi.create_token(params)
-    subscription.create_record(token.id)
-    redirect_to mypage_url, notice: '支払い情報を保存しました'
+    ActiveRecord::Base.transaction do
+      @user = current_user
+      @subscription = current_user.build_subscription
+      if @user.update(user_params)
+        begin
+          token = PayjpApi.create_token(params)
+        rescue Payjp::CardError => e
+          body = e.json_body
+          err = body[:error]
+          flash.now[:alert] = t("payjp.#{err[:code]}")
+          render :new and return
+        rescue => e
+          #TODO カード情報以外起因のエラー処理
+          flash.now[:alert] = "決済エラーが発生しました。"
+          render :new and return
+        end
+        @subscription.create_record(token.id)
+        redirect_to mypage_url, notice: '支払い情報を保存しました'
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.fetch(:user, {}).permit(
+      :phone_number, :l_name_kana, :f_name_kana
+    )
   end
 end
