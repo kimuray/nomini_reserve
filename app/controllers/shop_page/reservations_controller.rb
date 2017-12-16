c02f6dc52f0dd07248b0a0542b3aad7d1e0b7578
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-12-16 23:32:44 +0900

    店舗の予約画面でもアラカルトを選択できるように修正

--- app/controllers/shop_page/reservations_controller.rb
+++ app/controllers/shop_page/reservations_controller.rb
@@ -65,7 +65,7 @@ class ShopPage::ReservationsController < ShopPageController
   def reservation_params
     params.fetch(:reservation, {}).permit(
       :shop_id, :user_id, :reservation_category_id, :people_count,
-      :use_date, :use_time, :message, :status
+      :use_date, :use_time, :message, :status, :is_alacarte
     )
   end
 
