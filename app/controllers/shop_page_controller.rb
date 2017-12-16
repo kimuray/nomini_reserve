500ca65fa3ca2c2c740e915e01cf2d3fee2d10ee
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-12-17 00:22:46 +0900

    利用規約同意画面の作成

--- app/controllers/shop_page_controller.rb
+++ app/controllers/shop_page_controller.rb
@@ -3,4 +3,12 @@ class ShopPageController < ActionController::Base
 
   layout 'shop_page/application'
   before_action :authenticate_shop!
+
+  def request_agreement
+  end
+
+  def agree
+    current_shop.update(is_agree: true)
+    redirect_to shop_page_root_url, notice: '利用規約に同意しました'
+  end
 end
