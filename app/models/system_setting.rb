25f0777cb7964339be0d82b8312a5d797bb313c5
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-02-06 00:19:00 +0900

    設定値を各モデルで読み込むように修正

--- app/models/system_setting.rb
+++ app/models/system_setting.rb
@@ -1,2 +1,15 @@
 class SystemSetting < ApplicationRecord
+  validates :logical_name, presence: true
+  validates :physical_name, presence: true
+  validates :value, presence: true
+
+  class << self
+    def introduction_point
+      find_by(physical_name: 'introduction').value
+    end
+
+    def exchange_line
+      find_by(physical_name: 'exchange').value
+    end
+  end
 end
